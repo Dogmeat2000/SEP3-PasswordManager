749c3ff3103f1cc427df6bf29735a9dbef467f66
KatAamand <143415617+KatAamand@users.noreply.github.com>
2024-10-22 12:56:08 +0200

    Delete PM_Backendfra directory

--- PM_Backendfra/src/main/java/dk/sep3/passwordmanager/client/view/ICommandLineInterface.java
+++
@@ -1,6 +0,0 @@
-﻿package dk.sep3.passwordmanager.client.view;
-
-public interface ICommandLineInterface {
-  void start();
-  void handleCommand(String command);
-}
--- PM_Backendfra/src/main/java/dk/sep3/passwordmanager/loadBalancer/service/ILoadBalancerService.java
+++
@@ -1,9 +0,0 @@
-package dk.sep3.passwordmanager.loadBalancer.service;
-
-import dk.sep3.passwordmanager.loadBalancer.dto.ClientRequest;
-
-// Interface for handling and controlling client requests and sending them to the dbServer
-public interface ILoadBalancerService {
-    void handleLoad(ClientRequest request);
-    int getCurrentLoad();
-}
