30b7383eefc259a031d2e7464d14d240ba9b384d
KatAamand <katrine.aamand@icloud.com>
2024-10-23 11:33:07 +0200

    Updating dto's and ServerResponse to new standard and

--- PM_Backend/Common/src/main/java/common/ServerResponse.java
+++ PM_Backend/Common/src/main/java/common/ServerResponse.java
@@ -1,15 +1,23 @@
 package common;
 
+import common.dto.DTO;
+import common.dto.MasterUserDTO;
+
 public class ServerResponse {
     private String message;
     private int statusCode;
+    private DTO dto;
 
     // Tom konstruktør kræves for deserialisering
     public ServerResponse() {}
 
-    public ServerResponse(String message, int statusCode) {
-        this.message = message;
+    public ServerResponse(int statusCode, String message) {
         this.statusCode = statusCode;
+    }
+
+    public ServerResponse(int statusCode, DTO dto) {
+        this.statusCode = statusCode;
+        this.dto = dto;
     }
 
     public String getMessage() {
@@ -27,4 +35,13 @@ public class ServerResponse {
     public void setStatusCode(int statusCode) {
         this.statusCode = statusCode;
     }
+
+    public DTO getDto() {
+        return dto;
+    }
+
+    public void setDto(DTO dto) {
+        this.dto = dto;
+    }
+
 }
