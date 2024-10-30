5b02b1ce4ce760f14f503149fa0de27e929d69b3
Rasmus Sterup <69890797+Sterup15@users.noreply.github.com>
2024-10-30 09:02:01 +0100

    Cleaned up in Client - removed old project and replaced with 3 new: Shared, Client and BlazorFrontend. Ready to start work on the Client

--- PM_Backend/WebAPI/src/test/java/dk/sep3/webapi/WebApiApplicationTests.java
+++ PM_Backend/WebAPI/src/test/java/dk/sep3/webapi/WebApiApplicationTests.java
@@ -5,9 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WebApiApplicationTests {
-
+/*
     @Test
     void contextLoads() {
     }
 
+
+ */
 }
--- PM_Frontend/Client/src/main/C#/Logic/ICommandLineInterface.cs
+++
@@ -1,8 +0,0 @@
-namespace PM_Frontend.C#.Logic
-{
-    public interface ICommandLineInterface
-    {
-        void Start();
-        void HandleCommand(string command);
-    }
-}
