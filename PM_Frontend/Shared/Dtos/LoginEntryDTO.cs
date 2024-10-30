2cc1b78b2f7ba130f73d82cc17edc7c375746bbf
Rasmus Sterup <69890797+Sterup15@users.noreply.github.com>
2024-10-30 13:07:48 +0100

    Made C# DTO's again ;)

---
+++ PM_Frontend/Shared/Dtos/LoginEntryDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Shared.Dtos;
+
+public class LoginEntryDTO : DTO
+{
+    public string entryUsername { get; set; }
+    public string entryPassword { get; set; }
+    public int masterUserId { get; set; }
+    
+    public LoginEntryDTO(int id, string entryUsername, string entryPassword, int masterUserId) : base(id)
+    {
+        this.entryUsername = entryUsername;
+        this.entryPassword = entryPassword;
+        this.masterUserId = masterUserId;
+    }
+
+    public LoginEntryDTO(int id) : base(id)
+    {
+    }
+}
