bbddd4b95f7ecb20760b68432ed78db229368267
Marcus Grøne <marcusgrone@gmail.com>
2024-11-06 10:36:14 +0100

    Createing frontend

--- PM_Frontend/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ PM_Frontend/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7266a4319118b5e6f5558c9f4d774cf6ec82988f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53188c5ab6a29e511504a07e1c12fbaa48baf795")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
