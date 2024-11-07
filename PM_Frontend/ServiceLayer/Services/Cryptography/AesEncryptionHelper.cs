a86acf2b7ecb7209cb505c2359c91335893150e2
KatAamand <katrine.aamand@icloud.com>
2024-11-07 07:45:53 +0100

    Adding simple AES encryption

---
+++ PM_Frontend/ServiceLayer/Services/Cryptography/AesEncryptionHelper.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ServiceLayer.Services.Cryptography;
+
+/** AES encryption helper class.
+ * Uses symmetrical encryption AES (Advanced Encryption Standard) for encryption of sensitive data.
+ * The same key is used both for encryption and decryption *
+ */ 
+public class AesEncryptionHelper
+{
+    private static readonly byte[] Key = Encoding.UTF8.GetBytes("1234567890123456"); // Todo change to dynamic secure key, with at least 16 bytes
+
+    public static string Encrypt(string textToEncrypt)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Key;
+            aes.GenerateIV();
+            
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                memoryStream.Write(aes.IV, 0, aes.IV.Length);
+                
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                    {
+                        streamWriter.Write(textToEncrypt);
+                    }
+                    
+                    return Convert.ToBase64String(memoryStream.ToArray());
+                }
+            }
+        }
+    }
+
+    public static string Decrypt(string textToDecrypt)
+    {
+        byte[] fullCipher = Convert.FromBase64String(textToDecrypt);
+        
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Key;
+
+            byte[] iv = new byte[aes.BlockSize / 8]; 
+            Array.Copy(fullCipher, iv, iv.Length);
+            aes.IV = iv;
+
+            using (MemoryStream memoryStream = new MemoryStream(fullCipher, iv.Length, fullCipher.Length - iv.Length))
+            {
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader streamReader = new StreamReader(cryptoStream))
+                    {
+                        return streamReader.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
--- PM_Frontend/ServiceLayer/Services/Cryptography/CryptographyServiceImpl.cs
+++ PM_Frontend/ServiceLayer/Services/Cryptography/CryptographyServiceImpl.cs
@@ -4,27 +4,43 @@ namespace ServiceLayer.Services.Cryptography;
 
 public class CryptographyServiceImpl : ICryptographyService
 {
-    public async Task<MasterUserDTO> EncryptMasterUserAsync(MasterUserDTO masterUserDTO)
+    public async Task<MasterUserDTO> EncryptMasterUserAsync(MasterUserDTO masterUserDto)
     {
-        // TODO: Implement encryption logic
-        // For now, simply returning the input
-        return await Task.FromResult(masterUserDTO);
+        MasterUserDTO encryptedMasterUserDto = new();
+        
+        encryptedMasterUserDto.masterUsername = AesEncryptionHelper.Encrypt(masterUserDto.masterUsername);
+        encryptedMasterUserDto.masterPassword = AesEncryptionHelper.Encrypt(masterUserDto.masterPassword);
+        
+        return await Task.FromResult(encryptedMasterUserDto);
     }
 
-    public async Task<MasterUserDTO> DecryptMasterUserAsync(MasterUserDTO masterUserDTO)
+    public async Task<MasterUserDTO> DecryptMasterUserAsync(MasterUserDTO masterUserDto)
     {
-        // TODO: Implement decryption logic
-        // For now, simply returning the input
-        return await Task.FromResult(masterUserDTO);
+        MasterUserDTO decryptedMasterUserDto = new();
+        
+        decryptedMasterUserDto.masterUsername = AesEncryptionHelper.Decrypt(masterUserDto.masterUsername);
+        decryptedMasterUserDto.masterPassword = AesEncryptionHelper.Decrypt(masterUserDto.masterPassword);
+        
+        return await Task.FromResult(decryptedMasterUserDto);
     }
 
-    public async Task<LoginEntryDTO> EncryptLoginEntryAsync(LoginEntryDTO loginEntryDTO)
-    {
-        throw new NotImplementedException();
+    public async Task<LoginEntryDTO> EncryptLoginEntryAsync(LoginEntryDTO loginEntryDto)
+    {                                                                                
+        LoginEntryDTO encryptedLoginEntryDto = new();                                
+                                                                                     
+        encryptedLoginEntryDto.entryUsername = AesEncryptionHelper.Encrypt(loginEntryDto.entryUsername);
+        encryptedLoginEntryDto.entryPassword = AesEncryptionHelper.Encrypt(loginEntryDto.entryPassword);
+        
+        return await Task.FromResult(encryptedLoginEntryDto);
     }
 
-    public async Task<LoginEntryDTO> DecryptLoginEntryAsync(LoginEntryDTO loginEntryDTO)
+    public async Task<LoginEntryDTO> DecryptLoginEntryAsync(LoginEntryDTO loginEntryDto)
     {
-        throw new NotImplementedException();
+        LoginEntryDTO decryptedMasterUserDto = new();
+        
+        decryptedMasterUserDto.entryUsername = AesEncryptionHelper.Decrypt(loginEntryDto.entryUsername);
+        decryptedMasterUserDto.entryPassword = AesEncryptionHelper.Decrypt(loginEntryDto.entryPassword);
+        
+        return await Task.FromResult(decryptedMasterUserDto);
     }
 }
