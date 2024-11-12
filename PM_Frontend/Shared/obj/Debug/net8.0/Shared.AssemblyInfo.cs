430eca48d5702b38d281770ccf5809d714acb398
Rasmus Sterup <69890797+Sterup15@users.noreply.github.com>
2024-11-12 14:33:32 +0100

    Fixed method parameter

--- PM_Frontend/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ PM_Frontend/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86acf2b7ecb7209cb505c2359c91335893150e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f57082ee8930a1d0b55eaecc449734445e1a88ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
