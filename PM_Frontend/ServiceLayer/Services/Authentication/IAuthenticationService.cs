3603787ec042888ba40d694acb513a5632e01881
Marcus Grøne <marcusgrone@gmail.com>
2024-11-27 16:07:20 +0100

    Renmoved redundant auth services and cleandup code+comments.

--- PM_Frontend/ServiceLayer/Services/Authentication/IAuthenticationService.cs
+++
@@ -1,8 +0,0 @@
-using Shared.Dtos;
-
-namespace ServiceLayer.Services.Authentication;
-
-public interface IAuthenticationService
-{
-    Task<MasterUserDTO> AuthenticateUserAsync(string username, string password);
-}
